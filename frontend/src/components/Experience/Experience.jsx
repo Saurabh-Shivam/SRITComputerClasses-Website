44034cbcc4311b7a8e51cdd66104f985a2a32853
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-09-04 12:01:40 +0530

    Added experience compo

--- frontend/src/components/Experience/Experience.jsx
+++ frontend/src/components/Experience/Experience.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import "./Experience.css";
+// // for darkmode
+// import { themeContext } from "../../context/DarkMode/Context";
+// import { useContext } from "react";
 
+// This section consists of special achievements done by the User throughout his journey
 const Experience = () => {
-  return <div>I am Experience</div>;
+  // //for darkmode context
+  // const theme = useContext(themeContext);
+  // const darkMode = theme.state.darkMode;
+  return (
+    <div className="experience">
+      <div className="achievement">
+        <div
+          className="circle"
+          // for darkmode
+          // style={{ color: darkMode ? "var(--orange)" : "" }}
+        >
+          x+
+        </div>
+        {/* for darkmode */}
+        <span
+        // style={{ color: darkMode ? "white" : "" }}
+        >
+          years
+        </span>
+        <span>Experience</span>
+      </div>
+
+      <div className="achievement">
+        <div
+          className="circle"
+          // for darkmode
+          // style={{ color: darkMode ? "var(--orange)" : "" }}
+        >
+          y+
+        </div>
+        {/* for darkmode */}
+        <span
+        // style={{ color: darkMode ? "white" : "" }}
+        >
+          students
+        </span>
+        <span>Placed</span>
+      </div>
+
+      <div className="achievement">
+        <div
+          className="circle thirdCir"
+          // for darkmode
+          // style={{ color: darkMode ? "var(--orange)" : "" }}
+        >
+          z+
+        </div>
+        {/* for darkmode */}
+        <span
+        // style={{ color: darkMode ? "white" : "" }}
+        >
+          different
+        </span>
+        <span>Institutes Worked</span>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
