5ec504072762f3cb168fe157a8cad39621be61a5
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-09-04 12:35:44 +0530

    added course and courseCard compo

---
+++ frontend/src/components/CourseCard/CourseCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./CourseCard.css";
+// // for darkmode
+// import { themeContext } from "../../context/DarkMode/Context";
+// import { useContext } from "react";
+
+const CourseCard = ({ title, detail, review }) => {
+  //   //for darkmode context
+  //   const theme = useContext(themeContext);
+  //   const darkMode = theme.state.darkMode;
+  return (
+    // for darkmode
+    <div
+      className="coursecard"
+      //  style={{ background: darkMode ? "black" : "" }}
+    >
+      {/* <span style={{ color: darkMode ? "white" : "" }}>{title}</span>
+      <span style={{ color: darkMode ? "white" : "" }}>{detail}</span>
+      <span style={{ color: darkMode ? "white" : "" }}>{review}</span> */}
+      <span>{title}</span>
+      <span>{detail}</span>
+      <span>{review}</span>
+    </div>
+  );
+};
+
+// const CourseCard = (courses) => {
+//   return (
+//     <div className="coursecard">
+//       {courses.map((courses, key) => {
+//         const { id, title, detail, review } = courses;
+//         return (
+//           <div className="couCard" key={id}>
+//             <span>{courses.title}</span>
+//             <span>{courses.detail}</span>
+//             <span>{courses.review}</span>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+export default CourseCard;
--- frontend/src/components/Courses/Courses.jsx
+++ frontend/src/components/Courses/Courses.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+// import React, { useState, useEffect } from "react";
+import CourseCard from "../CourseCard/CourseCard";
+// import axios from "axios";
+import "./Courses.css";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div className="courses">
+      <CourseCard
+        title={"lang1"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"lang2"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"lang3"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"lang4"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"lang5"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"lang6"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"tally"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+
+      <CourseCard
+        title={"fullstack"}
+        detail={
+          "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
+        }
+        review={"very Lorem Ipsum has been the industry's standard"}
+      />
+      {/* <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard /> */}
+    </div>
+
+    // <div className="coursecard">
+    //   {courses.map((courses, key) => {
+    //     const { id, title, detail, review } = courses;
+    //     return (
+    //       <div className="couCard" key={id}>
+    //         <span>{title}</span>
+    //         <span>{detail}</span>
+    //         <span>{review}</span>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
+  );
 };
 
 export default Courses;
