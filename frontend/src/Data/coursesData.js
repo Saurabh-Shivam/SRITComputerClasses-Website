7cea1e32a2ea8eb2a073f277ccaf3aca6630fc41
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-09-04 22:29:51 +0530

    added coursesData file only

---
+++ frontend/src/Data/coursesData.js
@@ -0,0 +1,50 @@
+export const coursesData = [
+  {
+    title: "lang1",
+    detail:
+      "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "very Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang2",
+    detail:
+      "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "very Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang3",
+    detail:
+      "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "very Lfwegfweorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang4",
+    detail:
+      "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "very Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang5",
+    detail:
+      "industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "very Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang6",
+    detail:
+      "industry. Loremhtrhrth Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "veryhgdhh Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang7",
+    detail:
+      "industry. Loremhtrhrth Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "veryhgdhh Lorem Ipsum has been the industry's standard",
+  },
+  {
+    title: "lang8",
+    detail:
+      "industry. Loremhtrhrfwetsweth Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+    review: "veryhgdhh Lorem Ipsum has been the industry's standard",
+  },
+];
