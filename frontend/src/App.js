1d1b98839bbd22e6419f943c5b68cc22115cfd1f
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-09-14 12:12:22 +0530

    removed backend support(folder)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Courses from "./components/Courses/Courses";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import Login from "./components/Login/Login";
+// import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Copyright from "./components/Copyright/Copyright";
 // import ImageSlider from "./components/Gallery/ImageSlider";
@@ -23,7 +23,7 @@ function App() {
         <Route path="courses" element={<Courses />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="login" element={<Login />} />
+        {/* <Route path="login" element={<Login />} /> */}
         <Route path="register" element={<Register />} />
         {/* <Route path="gallery" element={<ImageSlider slides={SliderData} />} /> */}
         <Route path="gallery" element={<Gallery />} />
